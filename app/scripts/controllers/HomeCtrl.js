a8751539018b0d4ade9414b41e2cc8121d5e3d50
kimber <kimber.gee@gmail.com>
2016-09-27 14:49:12 -0400

    hide expired tasks, display task age on view, and refactor code

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,12 +1,62 @@
 (function() {
   function HomeCtrl($scope, $firebaseArray) {
-    var ref = firebase.database().ref().child('tasks');
-    $scope.tasks = $firebaseArray(ref);
-    $scope.addTask = function() {
-      $scope.tasks.$add({
-        text: $scope.newTaskText
-      });
+
+    var init = function() {
+      var ref = firebase.database().ref().child('tasks');
+      $scope.tasks = $firebaseArray(ref);
+      createBlankTask();
     };
+
+    // View Functions
+    $scope.addTask = function() {
+      $scope.tasks.$add($scope.newTask);
+      createBlankTask();
+    };
+
+    $scope.taskAge = function(task) {
+      return taskAge(task);
+    };
+
+    $scope.expiredTask = function(task) {
+      return expiredTask(task);
+    };
+
+    // Supporting Functions
+    var createBlankTask = function() {
+      $scope.newTask = {
+        text: "",
+        time: firebase.database.ServerValue.TIMESTAMP,
+        completed: false
+      };
+    }
+
+    var taskAge = function(task) {
+      var age = moment(task.time).startOf('hour').fromNow();
+      return age;
+    };
+
+/*  Ben's original code:
+    var getDaysOld = function(task) {
+      var now = new moment();
+      var date = new moment(task.time);
+      var diff = now.diff(date);
+      var duration = moment.duration(diff);
+      return duration.days();
+      return diff;
+    };
+*/
+
+    var expiredTask = function(task) {
+      var now = new moment();
+      var createdAt = new moment(task.time);
+      if ((now - createdAt) >= 604800000) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    init();
   }
 
   angular
